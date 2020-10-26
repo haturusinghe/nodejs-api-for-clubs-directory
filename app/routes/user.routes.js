6144413c4ba4d924dd675558d5989b07e6423a59
S.Haturusinghe <haturusingh3@gmail.com>
2020-10-26 11:04:24 +0530

    sample secure route added

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -1,4 +1,5 @@
 const passport = require("passport");
+const jwt = require("jsonwebtoken");
 
 module.exports = (app) => {
   app.post(
@@ -11,4 +12,27 @@ module.exports = (app) => {
       });
     }
   );
+
+  app.post("/login", async (req, res, next) => {
+    passport.authenticate("login", async (err, user, info) => {
+      try {
+        if (err || !user) {
+          const error = new Error("An error occurred.");
+
+          return next(error);
+        }
+
+        req.login(user, { session: false }, async (error) => {
+          if (error) return next(error);
+
+          const body = { _id: user._id, email: user.email };
+          const token = jwt.sign({ user: body }, "TOP_SECRET");
+
+          return res.json({ token });
+        });
+      } catch (error) {
+        return next(error);
+      }
+    })(req, res, next);
+  });
 };
