a410373814d8a1af5975d2846445ff2f16688fb2
S.Haturusinghe <haturusingh3@gmail.com>
2020-10-26 17:19:27 +0530

    list function at clubs get

--- app/routes/club.routes.js
+++ app/routes/club.routes.js
@@ -2,7 +2,7 @@ module.exports = (app) => {
   const clubs = require("../controllers/club.controller.js");
 
   // Create a new Note
-  app.post("/clubs", clubs.create);
+  app.post("/clubs", clubs.list);
 
   // Retrieve all clubs
   app.get("/clubs", clubs.findAll);
