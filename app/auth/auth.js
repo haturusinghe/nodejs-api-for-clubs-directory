cf828f1166c38a56daf8367105f89ff04bf145df
S.Haturusinghe <haturusingh3@gmail.com>
2020-10-26 11:03:50 +0530

    jwt strategy added

--- app/auth/auth.js
+++ app/auth/auth.js
@@ -2,6 +2,25 @@ const passport = require("passport");
 const localStrategy = require("passport-local").Strategy;
 const UserModel = require("../models/user.model");
 
+const JWTstrategy = require("passport-jwt").Strategy;
+const ExtractJWT = require("passport-jwt").ExtractJwt;
+
+passport.use(
+  new JWTstrategy(
+    {
+      secretOrKey: "TOP_SECRET",
+      jwtFromRequest: ExtractJWT.fromUrlQueryParameter("secret_token"),
+    },
+    async (token, done) => {
+      try {
+        return done(null, token.user);
+      } catch (error) {
+        done(error);
+      }
+    }
+  )
+);
+
 passport.use(
   "signup",
   new localStrategy(
