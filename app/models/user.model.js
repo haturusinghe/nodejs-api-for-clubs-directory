70a1e8ca586579f7134e09c0080a44c6b726f62f
S.Haturusinghe <haturusingh3@gmail.com>
2020-10-26 10:44:10 +0530

    updated user model

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const Schema = mongoose.Schema;
 
@@ -13,6 +14,21 @@ const UserSchema = new Schema({
     required: true,
   },
 });
+
+UserSchema.pre("save", async function (next) {
+  const user = this;
+  const hash = await bcrypt.hash(this.password, 10);
+
+  this.password = hash;
+  next();
+});
+
+UserSchema.methods.isValidPassword = async function (password) {
+  const user = this;
+  const compare = await bcrypt.compare(password, user.password);
+
+  return compare;
+};
 
 const UserModel = mongoose.model("user", UserSchema);
 
