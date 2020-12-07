dc82d98d028af6241364e946e0e928f37f673cba
S.Haturusinghe <haturusingh3@gmail.com>
2020-12-07 17:54:59 +0530

    updated filter() function

--- app/controllers/club.controller.js
+++ app/controllers/club.controller.js
@@ -73,18 +73,22 @@ exports.list = (req, res) => {
 };
 
 exports.filter = (req, res) => {
+  const {
+    name = "",
+    meetingDay = 0,
+    meetingLanguage = 0,
+    long = null,
+    latt = null,
+  } = req.query;
+  const searchObject = { $and: [] };
+  console.log(searchObject);
   let limit =
     req.query.limit && req.query.limit <= 100 ? parseInt(req.query.limit) : 10;
   let page = 0;
-  let name = "";
   if (req.query) {
     if (req.query.page) {
       req.query.page = parseInt(req.query.page);
       page = Number.isInteger(req.query.page) ? req.query.page : 0;
-    }
-    if (req.query.name) {
-      name = req.query.name;
-      console.log(name);
     }
   }
   ClubModel.list(limit, page, name).then((result) => {
